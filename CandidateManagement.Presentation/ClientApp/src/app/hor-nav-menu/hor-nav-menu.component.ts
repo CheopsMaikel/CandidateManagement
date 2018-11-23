b19bfc9e5c60e07a365eb88300baa6e409ce4b81
CheopsMaikel <maikel.gerardi@cheops.be>
2018-11-23 16:12:57 +0100

    First CandidateManagement version

---
+++ CandidateManagement.Presentation/ClientApp/src/app/hor-nav-menu/hor-nav-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-nav-menu',
+  templateUrl: './hor-nav-menu.component.html',
+  styleUrls: ['./hor-nav-menu.component.css']
+})
+export class HorNavMenuComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
